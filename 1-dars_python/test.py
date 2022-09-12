697c4b46369afaf185c51ebb248eb53a1af0ce1a
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 18:00:50 +0500

    Initial commit

---
+++ 1-dars_python/test.py
@@ -0,0 +1,6 @@
+"""print(" Salom Samarqand ")"""
+
+#Kvadrat tomoni a berilgan. Uning perimetri aniqlansin. P=4a
+a=float(input('Kvadrat tomonini kiriting: '))
+P=4*a
+print('Kvadrat perimetri: ',P)
